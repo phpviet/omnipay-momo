a954edb94b9d68b3b70ea3dd21a159e167c82a3c
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-21 14:17:17 +0700

    Added abstract http request.

---
+++ src/Message/AbstractHttpRequest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @link https://github.com/phpviet/omnipay-momo
+ * @copyright (c) PHP Viet
+ * @license [MIT](http://www.opensource.org/licenses/MIT)
+ */
+
+namespace Omnipay\MoMo\Message;
+
+use Omnipay\Common\Message\AbstractRequest;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+abstract class AbstractHttpRequest extends AbstractRequest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(): array
+    {
+        return $this->getParameters();
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws \Omnipay\Common\Exception\InvalidRequestException
+     */
+    public function initialize(array $parameters = []): self
+    {
+        parent::initialize($parameters);
+
+        foreach ($this->getHttpRequestData() as $key => $value) {
+            $this->setParameter($key, $value);
+            $this->validate($key);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Trả về mảng dữ liệu từ HTTP dùng để khởi tạo parameters.
+     * Nếu một trong các phần tử không tồn tại thì hãy thiết lập nó là NULL hổ trợ cho việc báo lỗi.
+     *
+     * @return array
+     */
+    abstract protected function getHttpRequestData(): array;
+}
