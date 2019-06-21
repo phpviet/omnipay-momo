91b7b9f987898e272b80f11933af173d4af58061
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-21 14:55:59 +0700

    Added query refund.

---
+++ src/Message/QueryRefundRequest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link https://github.com/phpviet/omnipay-momo
+ * @copyright (c) PHP Viet
+ * @license [MIT](http://www.opensource.org/licenses/MIT)
+ */
+
+namespace Omnipay\MoMo\Message;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class QueryRefundRequest extends AbstractRequest
+{
+    /**
+     * {@inheritdoc}
+     * @throws \Omnipay\Common\Exception\InvalidRequestException
+     */
+    public function getData(): array
+    {
+        $this->setParameter('requestType', 'refundStatus');
+
+        return parent::getData();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function responseClass(): string
+    {
+        return QueryRefundResponse::class;
+    }
+}
