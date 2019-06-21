ba7e024ee89cef353d2d04c01265758f42dc912d
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-21 14:56:08 +0700

    Added query transaction.

---
+++ src/Message/QueryTransactionRequest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @link https://github.com/phpviet/omnipay-momo
+ * @copyright (c) PHP Viet
+ * @license [MIT](http://www.opensource.org/licenses/MIT)
+ */
+
+namespace Omnipay\MoMo\Message;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class QueryTransactionRequest extends AbstractRequest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(): array
+    {
+        $this->setParameter('requestType', 'transactionStatus');
+
+        return parent::getData();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function responseClass(): string
+    {
+        return QueryTransactionResponse::class;
+    }
+}
