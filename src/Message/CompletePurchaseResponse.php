6f92b84a8aeb50a337009e056227cdaa6d5e007d
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-21 15:03:41 +0700

    Clean code.

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -13,6 +13,8 @@ namespace Omnipay\MoMo\Message;
  */
 class CompletePurchaseResponse extends AbstractResponse
 {
+    use Concerns\PurchasedResponseParameters;
+
     /**
      * Trả về thời gian phản hồi phiên giao dịch.
      *
@@ -21,16 +23,6 @@ class CompletePurchaseResponse extends AbstractResponse
     public function getResponseTime(): string
     {
         return $this->data['responseTime'];
-    }
-
-    /**
-     * Trả về giống với order id đã gửi lên MoMo, dùng để đối soát.
-     *
-     * @return string
-     */
-    public function getOrderId(): string
-    {
-        return $this->data['orderId'];
     }
 
     /**
@@ -44,26 +36,6 @@ class CompletePurchaseResponse extends AbstractResponse
     }
 
     /**
-     * Trả về giống với amount đã gửi lên MoMo, dùng để đối soát.
-     *
-     * @return string
-     */
-    public function getAmount(): string
-    {
-        return $this->data['amount'];
-    }
-
-    /**
-     * Trả về mã giao dịch của MoMo.
-     *
-     * @return string
-     */
-    public function getTransId(): string
-    {
-        return $this->data['transId'];
-    }
-
-    /**
      * Trả về giống với extra data đã gửi lên MoMo, dùng để đối soát.
      *
      * @return string
@@ -71,25 +43,5 @@ class CompletePurchaseResponse extends AbstractResponse
     public function getExtraData(): string
     {
         return $this->data['extraData'];
-    }
-
-    /**
-     * Trả về giống với partner code đã gửi lên MoMo, dùng để đối soát.
-     *
-     * @return string
-     */
-    public function getPartnerCode(): string
-    {
-        return $this->data['partnerCode'];
-    }
-
-    /**
-     * Trả về giống với access key đã gửi lên MoMo, dùng để đối soát.
-     *
-     * @return string
-     */
-    public function getAccessKey(): string
-    {
-        return $this->data['accessKey'];
     }
 }
