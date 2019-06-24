97b3bb2dbe38e6bd76a9e6464f5fdab7ee79b347
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-24 12:16:28 +0700

    Refactored AllInOne message.

--- src/Message/AllInOne/AbstractRequest.php
+++ src/Message/AllInOne/AbstractRequest.php
@@ -7,8 +7,6 @@
 
 namespace Omnipay\MoMo\Message\AllInOne;
 
-use InvalidArgumentException;
-use Omnipay\MoMo\Support\Signature;
 use Omnipay\MoMo\Message\AbstractRequest as BaseAbstractRequest;
 
 /**
@@ -17,6 +15,7 @@ use Omnipay\MoMo\Message\AbstractRequest as BaseAbstractRequest;
  */
 abstract class AbstractRequest extends BaseAbstractRequest
 {
+    use Concerns\RequestSignature;
     use Concerns\RequestParameters;
 
     /**
@@ -33,9 +32,9 @@ abstract class AbstractRequest extends BaseAbstractRequest
     public function getData(): array
     {
         $parameters = $this->getParameters();
-        $this->validate('partnerCode', 'accessKey', 'requestId', 'orderId', 'secretKey', 'requestType');
-        $parameters['signature'] = $this->generateSignature($parameters['requestType']);
         unset($parameters['secretKey']);
+        $this->validate('partnerCode', 'accessKey', 'requestId', 'orderId', 'secretKey', 'requestType');
+        $parameters['signature'] = $this->generateSignature();
 
         return $parameters;
     }
@@ -53,48 +52,5 @@ abstract class AbstractRequest extends BaseAbstractRequest
         $responseData = json_decode($response->getBody()->getContents(), true);
 
         return $this->response = new $responseClass($this, $responseData);
-    }
-
-    /**
-     * Trả về chữ ký điện tử gửi đến MoMo dựa theo `$requestType` truyền vào.
-     *
-     * @param  string  $requestType
-     * @return string
-     */
-    protected function generateSignature(string $requestType): string
-    {
-        $data = [];
-        $signature = new Signature($this->getParameter('secretKey'));
-
-        foreach ($this->getSignatureParameters($requestType) as $parameter) {
-            $data[$parameter] = $this->getParameter($parameter);
-        }
-
-        return $signature->generate($data);
-    }
-
-    /**
-     * Trả về danh sách param dùng để tạo chữ ký số theo `$requestType`.
-     *
-     * @param  string  $requestType
-     * @return array
-     */
-    protected function getSignatureParameters(string $requestType): array
-    {
-        switch ($requestType) {
-            case 'captureMoMoWallet':
-                return [
-                    'partnerCode', 'accessKey', 'requestId', 'amount', 'orderId', 'orderInfo', 'returnUrl', 'notifyUrl',
-                    'extraData',
-                ];
-            case 'transactionStatus':
-            case 'refundMoMoWallet':
-            case 'refundStatus':
-                return [
-                    'partnerCode', 'accessKey', 'requestId', 'orderId', 'requestType',
-                ];
-            default:
-                throw new InvalidArgumentException(sprintf('Request type: (%s) is not valid!', $requestType));
-        }
     }
 }
--- src/Message/AllInOne/Response.php
+++ src/Message/AllInOne/Response.php
@@ -7,10 +7,8 @@
 
 namespace Omnipay\MoMo\Message\AllInOne;
 
-use Omnipay\MoMo\Support\Signature;
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RequestInterface;
-use Omnipay\Common\Exception\InvalidResponseException;
 
 /**
  * @author Vuong Minh <vuongxuongminh@gmail.com>
@@ -18,6 +16,8 @@ use Omnipay\Common\Exception\InvalidResponseException;
  */
 class Response extends AbstractResponse
 {
+    use Concerns\ResponseSignatureValidation;
+
     /**
      * Khởi tạo đối tượng Response.
      *
@@ -30,9 +30,7 @@ class Response extends AbstractResponse
         parent::__construct($request, $data);
 
         if ($this->isSuccessful()) {
-            $parameters = $request->getParameters();
-
-            $this->validateSignature($parameters['secretKey']);
+            $this->validateSignature();
         }
     }
 
@@ -84,59 +82,5 @@ class Response extends AbstractResponse
     public function getTransactionReference(): ?string
     {
         return $this->data['transId'] ?? null;
-    }
-
-    /**
-     * Kiểm tra tính hợp lệ của dữ liệu do MoMo phản hồi.
-     *
-     * @param  string  $secretKey
-     * @throws InvalidResponseException
-     */
-    protected function validateSignature(string $secretKey): void
-    {
-        $data = [];
-        $signature = new Signature($secretKey);
-
-        foreach ($this->getSignatureParameters($this->data['requestType']) as $param) {
-            $data[$param] = $this->data[$param];
-        }
-
-        if (! $signature->validate($data, $this->data['signature'])) {
-
-            throw new InvalidResponseException(sprintf('Data signature response from MoMo is invalid!'));
-        }
-    }
-
-    /**
-     * Trả về danh sách các param data đã dùng để tạo chữ ký dữ liệu theo requestType truyền vào.
-     *
-     * @param  string  $requestType
-     * @return array
-     */
-    protected function getSignatureParameters(string $requestType): array
-    {
-        switch ($requestType) {
-            case 'captureMoMoWallet':
-                return [
-                    'requestId', 'orderId', 'message', 'localMessage', 'payUrl', 'errorCode', 'requestType',
-                ];
-            case 'transactionStatus':
-                return [
-                    'partnerCode', 'accessKey', 'requestId', 'orderId', 'errorCode', 'transId', 'amount', 'message',
-                    'localMessage', 'requestType', 'payType', 'extraData',
-                ];
-            case 'refundMoMoWallet':
-                return [
-                    'partnerCode', 'accessKey', 'requestId', 'orderId', 'errorCode', 'transId', 'message',
-                    'localMessage', 'requestType',
-                ];
-            case 'refundStatus':
-                return [
-                    'partnerCode', 'accessKey', 'requestId', 'orderId', 'errorCode', 'transId', 'amount', 'message',
-                    'localMessage', 'requestType',
-                ];
-            default:
-                return [];
-        }
     }
 }
